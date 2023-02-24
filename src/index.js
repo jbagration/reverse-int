8f46d0781605c1c9e9f5398b9d6a541d4f8832e9
jbagration <blissdery@gmail.com>
2023-02-24 16:33:27 +0500

    modified:   package-lock.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+    const reversed = parseInt(n.toString().split('').reverse().join(''));
+    return Math.abs(n) >= 0 ? reversed : -reversed;
+  };
   
-}
